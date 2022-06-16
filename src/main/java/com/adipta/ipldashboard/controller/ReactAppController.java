730caf559972924f7aff5641b5ca967dc1a52b29
adiptabasu <adiptabasu28@gmail.com>
2022-06-17 01:49:10 +0530

    Committed for React controller

---
+++ src/main/java/com/adipta/ipldashboard/controller/ReactAppController.java
@@ -0,0 +1,18 @@
+package com.adipta.ipldashboard.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactAppController 
+{
+
+    @RequestMapping(value = {"/", "/{x:[\\w\\-]+}", "/{x:^(?!team$).*$}/*/{y:[\\w\\-]+}","/error"  })
+    public String getIndex(HttpServletRequest request) 
+    {
+        return "/index.html";
+    }
+
+}
